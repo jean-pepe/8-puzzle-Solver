f37a256ee7856a21f10701f7e3d0363c8f3c9f0e
jean-pepe <109477682+jean-pepe@users.noreply.github.com>
2022-07-23 16:16:41 +0200

    add java File

---
+++ Board.java
@@ -0,0 +1,190 @@
+import java.util.*;
+
+public class Board
+{
+    private final int boardDimension;
+    private final char[] cube;
+    private int voidPosition;
+   
+    private int row(int p) {
+        return (int) Math.ceil((double)p/(double)boardDimension);
+    }
+   
+    private int column(int p) {
+        if (p % boardDimension == 0) 
+            return boardDimension;
+        return p % boardDimension;
+    }
+
+    public Board(int[][] cubes) {
+        boardDimension = cubes.length;
+        this.cube = new char[boardDimension*boardDimension];
+        int k = 0 ;
+        for (int i = 0; i < boardDimension; i++){
+            for (int j = 0; j < boardDimension; j++) {
+                this.cube[k] = (char) cubes[i][j];
+                if (cubes[i][j] == 0) voidPosition = k;
+                k ++;
+            }
+        }
+    }
+
+    public int dimension() { return boardDimension; }
+
+    public int manhattan() {
+        int manhattan = 0;
+        for (int k = 0; k < boardDimension*boardDimension; k++) {
+            if (cube[k] == 0) continue;
+            int rowdifference = Math.abs(row(cube[k]) - row(k+1));
+            int columndiff = Math.abs(column(cube[k]) - column(k+1));
+            manhattan = manhattan + rowdifference + columndiff;
+        }
+        return manhattan;
+    }
+
+    public boolean isGoal() {
+        for (int k = 0 ; k < boardDimension * boardDimension - 2; k++) {
+            if (cube[k] > cube[k + 1]) return false;
+        }
+        return true;
+    }
+    
+    public Board twin() 
+    {
+        boolean swapsucc = false;
+        char[] clone = cube.clone();
+        int k = 0;
+        do { k=(int)(Math.random()*(boardDimension*boardDimension)); } 
+        while (cube[k] == 0);
+
+        while (swapsucc == false) {
+            int choice = (int) (Math.random()*4);
+            switch(choice) {
+                case 0 :
+                    if(row(k+1) == 1) swapsucc = false;
+                    else if(clone[k-boardDimension] == 0) swapsucc = false;
+                    else {
+                        swapUp(clone,k);
+                        swapsucc = true;
+                    }
+                    break;
+                case 1 : //swap down
+                    if(row(k+1) == boardDimension) swapsucc = false;
+                    else if(clone[k+boardDimension] == 0) swapsucc = false;
+                    else {
+                        swapDown(clone,k);
+                        swapsucc = true;
+                    }
+                    break;
+                case 2 : //swap left
+                    if(column(k+1) == 1) swapsucc = false;
+                    else if (clone[k-1] == 0) swapsucc = false;
+                    else {
+                        swapLeft(clone,k);
+                        swapsucc = true;
+                    }
+                    break;
+                case 3 : //swap right
+                    if(column(k+1) == boardDimension) swapsucc = false;
+                    else if(clone[k+1] == 0) swapsucc = false;
+                    else {
+                        swapRight(clone,k);
+                        swapsucc = true;
+                    }
+                    break;
+            }
+        }
+        Board cloneBoard = new Board(matrix(clone));
+        return cloneBoard;
+    }
+
+    public boolean equals(Object t) 
+    {
+        if (t == this) return true;
+        if (t == null) return false;
+        if (t.getClass() != this.getClass()) return false;
+        Board that = (Board) t;
+        if (!Arrays.equals(this.cube, that.cube)) return false;
+        return true;
+    }
+ 
+    public Iterable<Board> neighbors() 
+    {
+        Stack<Board> stackNeighbors = new Stack<Board>();
+        char[] neighbor;
+        if(row(voidPosition+1) != 1) 
+        {
+            neighbor = cube.clone();
+            swapUp(neighbor,voidPosition);
+            Board neighborBoard = new Board(matrix(neighbor));
+            stackNeighbors.push(neighborBoard);
+        }
+        if(row(voidPosition+1) != boardDimension) 
+        {
+            neighbor = cube.clone();
+            swapDown(neighbor,voidPosition);
+            Board neighborBoard = new Board(matrix(neighbor));
+            stackNeighbors.push(neighborBoard);
+        }
+        if(column(voidPosition+1) != 1) 
+        {
+            neighbor = cube.clone();
+            swapLeft(neighbor,voidPosition);
+            Board neighborBoard = new Board(matrix(neighbor));
+            stackNeighbors.push(neighborBoard);
+        }
+        if(column(voidPosition+1) != boardDimension) 
+        {
+            neighbor = cube.clone();
+            swapRight(neighbor,voidPosition);
+            Board neighborBoard = new Board(matrix(neighbor));
+            stackNeighbors.push(neighborBoard);
+        }
+        return stackNeighbors;
+    }
+
+
+    public String toString(){
+        StringBuilder s = new StringBuilder();
+        int m = 0;
+        for (int i = 0; i < (boardDimension*boardDimension); i++){
+    
+                s.append(String.format("%2d ", (int)cube[m]));
+                m++;
+        }
+        return s.toString();
+    }
+   
+    private void swapUp(char[] y, int k){
+        char temp = y[k];
+        y[k] = y[k - boardDimension];
+        y[k - boardDimension] = temp;
+    }
+    private void swapDown(char[] y, int k){
+        char temp = y[k];
+        y [k]= y[k + boardDimension];
+        y[k + boardDimension] = temp;
+    }
+    private void swapLeft(char[] y, int k){
+        char temp = y[k];
+        y[k] = y[k - 1];
+        y[k - 1] = temp;
+    }
+    private void swapRight(char[] y, int k){
+        char temp = y[k];
+        y[k] = y[k + 1];
+        y[k + 1] = temp;
+    }
+
+    private int[][] matrix(char[] y) 
+    {
+        int k = 0 ;
+        int[][] cubes = new int[boardDimension][boardDimension];
+        for (int i = 0; i < boardDimension; i++)
+            for (int j = 0; j < boardDimension; j++) {
+            cubes[i][j] = y[k];
+            k++;
+        }
+        return cubes;
+    }    
+}
---
+++ Solver.java
@@ -0,0 +1,118 @@
+import java.io.*;
+import java.util.*;
+
+public class Solver{
+    private PriorityQueue<SearchNode>  priorityq;
+    private PriorityQueue<SearchNode> pqtwin;
+    private int n;
+    private Board init;
+    private Board goal;
+    private SearchNode fine;
+
+    private class SearchNode implements Comparable<SearchNode>{
+        private Board board;
+        private int m;
+        private int priority;
+        private SearchNode previousN;
+
+        public SearchNode(Board board, int m, SearchNode previousN){
+            this.board = board;
+            this.m = m;
+            priority = m + board.manhattan();
+            this.previousN = previousN;
+        }
+
+        public int compareTo(SearchNode that) {
+            return (this.priority - that.priority);
+        }
+    }
+    
+    public Solver(Board init){        
+        if(init == null) throw new  NullPointerException();
+
+        this.init = init;
+        n = init.dimension();
+        priorityq = new PriorityQueue<SearchNode>();
+        pqtwin = new PriorityQueue<SearchNode>();
+
+        int[][] cubes = new int[n][n];
+        int k = 1 ;
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++) {
+            cubes[i][j] = k;
+            k++;
+        }
+        cubes[n-1][n-1] = 0;
+        goal = new Board(cubes);
+
+        SearchNode minN;
+        SearchNode minNTwin;
+        priorityq.add(new SearchNode(init, 0, null));
+        pqtwin.add(new SearchNode(init.twin(), 0, null));
+        while(!priorityq.peek().board.equals(goal) && !pqtwin.peek().board.equals(goal)) {
+            minN = priorityq.peek();
+            minNTwin = pqtwin.peek();
+            priorityq.poll();  
+            pqtwin.poll();
+            for (Board neighbor: minN.board.neighbors()){
+                if (minN.m == 0) 
+                    priorityq.add(new SearchNode(neighbor, minN.m+1, minN));
+                else if (!neighbor.equals(minN.previousN.board)) 
+                    priorityq.add(new SearchNode(neighbor, minN.m+1, minN));
+            }
+            
+            for (Board neighbor: minNTwin.board.neighbors()) {
+                if (minNTwin.m == 0) 
+                    pqtwin.add(new SearchNode(neighbor, minNTwin.m+1, minNTwin));
+                else if (!neighbor.equals(minNTwin.previousN.board)) 
+                    pqtwin.add(new SearchNode(neighbor, minNTwin.m+1, minNTwin));
+            }
+        }
+    }
+   
+    public boolean isSoluble(){
+        if (priorityq.peek().board.equals(goal))
+            return true;
+        if (pqtwin.peek().board.equals(goal))
+            return false;
+        return false;
+    }
+
+    public int moves() {
+        if(!isSoluble()) 
+            return -1;
+        return priorityq.peek().m;
+    }
+
+    public Iterable<Board> listSolution() {
+        LinkedList<Board> stackSolution = new LinkedList<Board>();      
+        SearchNode curr = priorityq.peek();
+        while (curr.previousN!=null) {
+            stackSolution.addFirst(curr.board);
+            curr = curr.previousN;
+        }
+        stackSolution.addFirst(init);
+        return stackSolution;
+    }
+
+    public static void main(String[] args) {
+        String nomefile = args[0];
+        try {      
+            FileReader file = new FileReader(nomefile);
+            Scanner scan = new Scanner(file);           
+            int n = scan.nextInt();
+            int[][] cubes = new int[n][n];
+            for (int i = 0; i < n; i++)    
+                for (int j = 0; j < n; j++){
+                    cubes[i][j] = scan.nextInt();
+                }
+            Board init= new Board(cubes);
+            Solver solving = new Solver(init);
+            System.out.println("Number of moves = " + solving.moves());
+            for (Board board : solving.listSolution())
+                System.out.println(board.toString());
+             file.close();         
+        } 
+        catch (Exception e) {}
+        }
+    }
